b5ec99b651f2ebca9c8275dc16fdb4e905e7f98a
dorianprogrammer <dorianrr98@gmail.com>
2021-05-30 09:57:50 -0600

    Initial commit

---
+++ functions/util/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyBQqDam4IjijMsIAWnfJRsOeNHStgzwv8Y",
+  authDomain: "socialape-c9430.firebaseapp.com",
+  projectId: "socialape-c9430",
+  storageBucket: "socialape-c9430.appspot.com",
+  messagingSenderId: "39714111719",
+  appId: "1:39714111719:web:72d29e5c4bdf961debe242",
+  measurementId: "G-9E7HY04E6W",
+};
